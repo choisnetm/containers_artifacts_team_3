663883a9818a3b30339e0d9069d2cd53141f2280
Mickael CHOISNET <mickael.choisnet@ocea-sb.com>
2022-12-07 14:08:16 +0100

    add secrets as kubectl cmd

---
+++ scripts/aks-secrets.ps1
@@ -0,0 +1,7 @@
+kubectl create secret `
+    generic sql-secrets `
+    --from-literal ASPNETCORE_ENVIRONMENT=Development `
+    --from-literal SQL_SERVER=sqlservernom9737.database.windows.net `
+    --from-literal SQL_DBNAME=mydrivingDB `
+    --from-literal SQL_USER=sqladminnOm9737 `
+    --from-literal SQL_PASSWORD=Password123!
