08e40705a9d5cb803e462f2028f458bee8c7676a
ANDRE David <david.andre@ocea-sb.com>
2022-12-08 10:57:19 +0100

    edit aks create

--- scripts/aks-create.ps1
+++ scripts/aks-create.ps1
@@ -6,6 +6,8 @@ $rgName = 'teamResources'
 $dockerRegistryServer = 'registrynom9737.azurecr.io'
 $acrLogin = 'registrynom9737'
 $acrPassword = 'ArwO+4WI1pBwTVOWCzdlrfOckT00o+9z'
+$vnetSubnetId = '/subscriptions/c56594cd-3b57-4a5b-9cea-eebd774fdddd/resourceGroups/teamResources/providers/Microsoft.Network/virtualNetworks/vnet/subnets/aks-subnet'
+$admonGroupObjectIds = 09f565d5-d7ed-4bf8-b8c3-8fd22b80dc23
 
 # $aksExist = az aks show --name $aksName --resource-group $rgName
 
@@ -18,6 +20,24 @@ $acrPassword = 'ArwO+4WI1pBwTVOWCzdlrfOckT00o+9z'
 # --enable-msi-auth-for-monitoring  `
 # --generate-ssh-keys `
 # --attach-acr $acrName
+
+# az aks create `
+# --resource-group $rgName `
+# --name $aksName `
+# --enable-managed-identity `
+# --node-count 1 `
+# --enable-addons monitoring `
+# --enable-msi-auth-for-monitoring
+# --network-plugin azure `
+# --vnet-subnet-id $vnetSubnetId `
+# --generate-ssh-keys `
+# --docker-bridge-address 172.17.0.1/16 `
+# --dns-service-ip 10.0.0.10 `
+# --service-cidr 10.0.0.0/16 `
+# --enable-private-cluster `
+# --attach-acr $acrName `
+# --aad-admin-group-object-ids $admonGroupObjectIds `
+# --enable-aad
 
 # az aks update -n $aksName -g $rgName --attach-acr $acrName
 
