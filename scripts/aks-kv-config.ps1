efd142c160fb28d71cd51917b8e8a20f59dc2879
Mickael CHOISNET <mickael.choisnet@ocea-sb.com>
2022-12-08 11:29:59 +0100

    create kv and config

--- scripts/aks-kv-config.ps1
+++ scripts/aks-kv-config.ps1
@@ -9,3 +9,11 @@ az keyvault secret set --vault-name aks-openhack-team3-kv -n SqlName --value myd
 az keyvault secret set --vault-name aks-openhack-team3-kv -n SqlUser --value sqladminnOm9737
 az keyvault secret set --vault-name aks-openhack-team3-kv -n SqlPassword --value Password123!
 
+$id = az aks show -g teamResources -n openhack-aks-challenge3-bis --query addonProfiles.azureKeyvaultSecretsProvider.identity.clientId -o tsv
+
+# set policy to access keys in your key vault
+az keyvault set-policy -n aks-openhack-team3-kv --key-permissions get --spn $id
+# set policy to access secrets in your key vault
+az keyvault set-policy -n aks-openhack-team3-kv --secret-permissions get --spn $id
+# set policy to access certs in your key vault
+az keyvault set-policy -n aks-openhack-team3-kv --certificate-permissions get --spn $id
